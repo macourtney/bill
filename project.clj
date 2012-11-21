a8d22098cebc0f5b3778b148fcdb7abeca4a9667
Matthew Courtney <macourtney@gmail.com>
2012-11-21 15:40:35 -0500

    Created the initial leiningen project.clj file.

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject bill "0.0.1"
+  :description "Bill the builder is a build tool for clojure. Unlike leiningen, bill uses the hash code of a jar file to determine the authenticity of the jar."
+  :dependencies [[org.clojure/clojure "1.4.0"]])
